f0dc444cd0578fcebcc2e7ce0b4bf081bcdab2c5
Urte Žarnauskaitė <urte.yar@gmail.com>
2020-10-12 11:42:21 +0300

    education & experience data

---
+++ js/education.js
@@ -0,0 +1,35 @@
+const edu = [
+    {   
+        year: '2010 - 2014',
+        university: 'Studied At Cambridge University',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur non reiciendis aut quo.'
+    },
+    {
+        year: '2014 - 2016',
+        university: 'Master In Computer Science',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur non reiciendis aut quo.'
+  
+    },
+    {
+        year: '2016 - 2018',
+        university: 'UI/UX Designer',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur non reiciendis aut quo.'
+    },
+    {
+        year: '2010 - 2014',
+        university: 'Senior Graphic Designer',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur non reiciendis aut quo.'
+    },
+    {
+        year: '2014 - 2016',
+        university: 'Web Developer',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur non reiciendis aut quo.'
+    },
+    {
+        year: '2016 - 2018',
+        university: 'Freelancer And Themeforest',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur non reiciendis aut quo.'
+    }
+];
+
+export default edu;
