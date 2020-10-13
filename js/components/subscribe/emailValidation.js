359e7ef4674740e6a6287323147580427c5a1882
ugniusnor <ugnn97@gmail.com>
2020-10-13 11:36:11 +0300

    error icon added when validating email

--- js/components/subscribe/emailValidation.js
+++ js/components/subscribe/emailValidation.js
@@ -3,16 +3,23 @@ const email = document.querySelector(".subscribe-input")
 const emailError = document.querySelector(".subscribe-error")
 let message=[];
 let form = document.querySelector("form")
+const SuberrorIcon = document.querySelector(".subscribe-errorIcon")
 form.addEventListener('submit', (e)=> {
   if ( !email.value.match (/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
     message.push("It doesn't look like an email");
         e.preventDefault();
+        SuberrorIcon.style.display = "block";
+  }
+  else {
+    SuberrorIcon.style.display = "none";
+    emailError.style.display="none";
   }
 if(message.length > 0) {
 
     emailError.innerHTML = message;
 }
  message=[];
+
 })
 
 }
