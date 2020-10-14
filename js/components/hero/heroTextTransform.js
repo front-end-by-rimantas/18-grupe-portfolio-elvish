b88f820e1f6ebb55d69d29e6e385de8e704f14c7
ugniusnor <ugnn97@gmail.com>
2020-10-14 09:31:43 +0300

    Milda requests updated

--- js/components/hero/heroTextTransform.js
+++ js/components/hero/heroTextTransform.js
@@ -1,30 +1,7 @@
-// const text = document.querySelector(".changing-text")
-// let textList=["Elvish Walter", "a developer", "a designer"];
-// // function textChanging (){
-// //     developer();
-// //     // setInterval(function(){ developer();},3000);
-// //     // setInterval(function(){ Elvish();}, 6050);
-// //     // setInterval(function(){ designer();}, 9000);
-// //     }
-
-// //     function Elvish () {
-// //         console.log("start");
-// // return text.innerHTML= "" + textList[0];
-// //     }
-//     function developer (){
-//         console.log("finish");
-//         return  text.innerHTML=textList[1];
-//     }
-//     // function designer (){
-//     //     return   text.innerHTML=textList[2];
-//     // }
-
-//     export default developer;
 
 let textList = ["a developer", "a designer","Elvish Walter"];
 let counter = 0;
 let changingPart = document.querySelector(".changing-text");
-console.log(changingPart);
 setInterval(heroTextTransform, 4000);
 
 function heroTextTransform() {
