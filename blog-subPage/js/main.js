d3df309b1b9c0c7c03e2ecf6e6d751827483700e
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2020-10-16 18:39:34 +0300

    Header dropdown

--- blog-subPage/js/main.js
+++ blog-subPage/js/main.js
@@ -1,9 +1,15 @@
 import { fixingHeader } from './components/header/stickyHeader.js';
+import { burgerDropdpwn } from './components/header/burgerDropdown.js';
 
 import { blogData } from './data/blog.js';
 import { renderBlog } from './components/blog/renderBlog.js';
 
 fixingHeader();
+burgerDropdpwn();
 
 // console.log(blogData);
 renderBlog(blogData);
+
+
+
+
