0e227a3d692de074898e8809279cef5815d8badb
Karina-N <nektarinas@gmail.com>
2020-10-19 22:31:38 +0300

    animation in gallery

--- js/components/gallery/generateGalleryItem.js
+++ js/components/gallery/generateGalleryItem.js
@@ -5,9 +5,11 @@ function generateGalleryItem(data, imagePath) {
     HTML.className = 'gallery-image col-4 col-md-6 col-sm-12';
     HTML.innerHTML = ` 
                     <div class="overlay">
+                        <div class="overlay-bg"></div>
                         <div class="gallery-image-titles">
-                            <div id="h3"><h3>${data.title}</h3></div>
-                            <div id="h4"><h4>${data.content}</h4></div>
+
+                            <h3>${data.title}</h3>
+                            <h4>${data.content}</h4>
                         <div>
                     </div>`
 
