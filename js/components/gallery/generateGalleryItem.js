5e3e0816cdd181c570849e1db6c134d93dea209b
Karina-N <nektarinas@gmail.com>
2020-10-19 09:40:46 +0300

    updt

--- js/components/gallery/generateGalleryItem.js
+++ js/components/gallery/generateGalleryItem.js
@@ -2,17 +2,17 @@ function generateGalleryItem(data, imagePath) {
 
     
     const HTML = document.createElement('div');
-    HTML.className = 'gallery-image';
+    HTML.className = 'gallery-image col-4 col-md-6 col-sm-12';
     HTML.innerHTML = ` 
                     <div class="overlay">
                         <div class="gallery-image-titles">
-                            <h3>${data.title}</h3>
-                            <h4>${data.content}</h4>
+                            <div id="h3"><h3>${data.title}</h3></div>
+                            <div id="h4"><h4>${data.content}</h4></div>
                         <div>
                     </div>`
 
                 HTML.style.backgroundImage = "url('" + imagePath + data.image  + "')";
-                // HTML.querySelector('.overlay').style.backgroundImage = 
+                // HTML.querySelector('.overlay').style.backgroundImage = // jeigu background deciau gilesniam lygyje
 
     return HTML;
                 
