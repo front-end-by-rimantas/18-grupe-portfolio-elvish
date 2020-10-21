23d46128b2d1b14b10fbd3f33f8b8bb325158897
ugniusnor <ugnn97@gmail.com>
2020-10-21 14:10:10 +0300

    dots working!!!

--- js/components/header/scrollFromTop.js
+++ js/components/header/scrollFromTop.js
@@ -6,17 +6,20 @@ function scrollFromTop () {
     for ( let i=0; i < navArray.length-1;i++) {
        
         window.addEventListener('scroll', function (){
-            if ( window.scrollY >= sectionHeights[i].height && window.scrollY < sectionHeights[i+1].height ) {
+            let currentSection = document.getElementById(sectionHeights[i].name)
+            let nextSection = document.getElementById(sectionHeights[i+1].name)
+            if ( window.scrollY+100 >= currentSection.offsetTop && window.scrollY+100 < nextSection.offsetTop ) {
+                // document.getElementById("education").offsetTop
                 navArray[i].classList.add("single-dot")
             }
             else navArray[i].classList.remove("single-dot")
         })
     }
-    for ( let i=0;i<navArray.length;i++) {
-        navArray[i].addEventListener('click', function (){
-            console.log(window.scrollY);
-            console.log(sectionHeights[i].height);
-        })
-    }
+    // for ( let i=0;i<navArray.length;i++) {
+    //     navArray[i].addEventListener('click', function (){
+    //         console.log(window.scrollY);
+    //         console.log(sectionHeights[i].height);
+    //     })
+    // }
 }
 export default scrollFromTop;
--- js/data/section-heigths.js
+++ js/data/section-heigths.js
@@ -3,35 +3,30 @@
 const sectionHeights = [
     {
         name: "home",
-        height: document.getElementById("home").offsetTop
-    },
+},
     {
         name: "about",
-        height: document.getElementById("about").offsetTop
-    },
+     },
     {
         name: "services",
-        height: document.getElementById("services").offsetTop
-    },
+            },
     {
         name: "education",
-        height: document.getElementById("education").offsetTop
+        
     },
     {
         name: "work",
-        height: (document.getElementById("work").offsetTop+70)
-    },
+},
     {
         name:"client",
-        height: document.getElementById("client").offsetTop
-    },
+          },
     {
         name: "blog",
-        height: document.getElementById("blog").offsetTop//reiks papildyti kai turesim kolkas cia placeholderis
+    
     },
     {
         name:"contact",
-        height: document.getElementById("contact").offsetTop //papildyti
+ 
     }
 ]
 export default sectionHeights;
