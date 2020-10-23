cbfd2e2b8d8a5bc7fe04bf68efbd03ffd3157e0b
Urte Žarnauskaitė <urte.yar@gmail.com>
2020-10-23 11:17:55 +0300

    fixed validation

--- js/components/contact-us/formValidation.js
+++ js/components/contact-us/formValidation.js
@@ -7,6 +7,7 @@ function contactFormValidation() {
     const message = document.querySelector("#message");
     const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const letters = /^[A-Za-z]+$/;
+    // const alphanumeric = /^[0-9a-zA-Z]+$/;
 
 
     messForm.addEventListener('submit', (e) => {
@@ -27,7 +28,7 @@ function contactFormValidation() {
             //show error
             //add error class
             setErrorFor(name, 'Name cannot be blank');
-        } else if (nameValue.length < 2 && nameValue != letters) {
+        } else if (nameValue.length < 2 || !nameValue.match(letters)) {
             setErrorFor(name, "Please enter valid name");
         } else {
             //add success class
@@ -38,7 +39,7 @@ function contactFormValidation() {
 
         if (emailValue === '' ) {
             setErrorFor(email, 'Email cannot be blank');
-        } else if (emailValue.length < 6 && email != emailFormat) {
+        } else if (email != emailFormat) {
             setErrorFor(email, "Please enter valid email");
         } else {
             setSuccessFor(email);
@@ -48,7 +49,7 @@ function contactFormValidation() {
 
         if (subjectValue === '' ) {
             setErrorFor(subject, 'Subject cannot be blank');
-        } else if (subjectValue.length < 5 && subjectValue != 'string') {
+        } else if (subjectValue.length < 5 || !subjectValue.match(letters)) {
             setErrorFor(subject, "Please enter valid subject");
         } else {
             setSuccessFor(subject);
@@ -58,7 +59,7 @@ function contactFormValidation() {
 
          if (messageValue === '' ) {
             setErrorFor(message, 'Message cannot be blank');
-        } else if (messageValue.length < 10 && message.length > 140 && messageValue != 'string') {
+        } else if (messageValue.length < 10 || messageValue.length > 300 || !messageValue.match(letters)) {
             setErrorFor(message, "Please enter valid message");
         } else {
             setSuccessFor(message);
