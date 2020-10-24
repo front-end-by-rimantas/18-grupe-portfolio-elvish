9500192d9e3e71836e8b3539dd918a5aced186ac
mildasuziedelyte <mildasuziedelyte@users.noreply.github.com>
2020-10-24 23:24:49 +0300

    updt

--- js/components/trusties/newTrusties.js
+++ js/components/trusties/newTrusties.js
@@ -1,7 +1,7 @@
     import trustieData from "../../data/dataTrusties.js"
 
     function renderSignleTrustieAvatar(trustieData) {
-        return `<div class="${trustieData.id}"> <img src="./img/client-logos/${trustieData.img}" alt=" Client logo"> </div> `;
+        return `<div class="col-2 col-lg-4 col-md-12 ${trustieData.id}"> <img src="./img/client-logos/${trustieData.img}" alt=" Client logo"> </div> `;
     };
 
     export default renderSignleTrustieAvatar;
